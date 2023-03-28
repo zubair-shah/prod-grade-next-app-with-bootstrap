5312522370b73e57a918a7fe7cd84830cd6745ee
zubair-shah <shahzubair269@gmail.com>
2023-03-28 17:00:22 +0500

    bootstrap added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+  return (
+    <>
+       <Component {...pageProps} />
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"/>
+    </>
+  )
 }
